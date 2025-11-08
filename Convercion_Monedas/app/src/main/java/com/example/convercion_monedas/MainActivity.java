7a41923dc1ec0690be8cedd2dd9d6c309f17cbf5
Fabianlv27 <fabianlv1920@gmail.com>
2025-11-08 20:13:29 +0100

    Convercion monedas

---
+++ Convercion_Monedas/app/src/main/java/com/example/convercion_monedas/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.convercion_monedas;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    double[] cambios={1,0.86,153.53,0.76};
+    boolean updating = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    for(int i=1;i<4;i++){
+        int resId=getResources().getIdentifier("eq_0"+"_"+i,"id",getPackageName()) ;
+        EditText e=findViewById(resId);
+        e.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!updating){
+                    Rellenar();
+                }
+
+            }
+        });
+    }
+Rellenar();
+    }
+public void Rellenar(){
+        updating=true;
+        for (int i=0;i<4;i++){
+            for (int j = 0; j < 4; j++) {
+                int resId=getResources().getIdentifier("eq_"+i+"_"+j,"id",getPackageName()) ;
+                EditText  e=findViewById(resId);
+                if (e==null) continue;
+               double valor=cambios[j]/cambios[i];
+               e.setText(String.valueOf(valor) );
+               if (i==j){
+                   e.setEnabled(false);
+                   e.setBackgroundColor(Color.LTGRAY);
+               }
+               if(i!=0){
+                   e.setEnabled(false);
+               }
+            }
+        }
+        updating=false;
+}
+}
